e60d253cadfe6d4d8ff62378d7ce4b8161a37452
Preetham <preetham93m@gmail.com>
2025-05-19 11:19:48 +0530

    azure pipline example

---
+++ Javateam/src/main/java/com/preetham/javateam/App.java
@@ -0,0 +1,13 @@
+package com.preetham.javateam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
